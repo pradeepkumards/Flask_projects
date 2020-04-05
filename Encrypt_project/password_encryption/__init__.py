e44dac3b944baa11ec2e035f40360d97a9909f50
PradeepDS <ppradeepkumards@gmail.com>
2020-04-05 17:55:57 +0000

    adding initial code

---
+++ Encrypt_project/password_encryption/__init__.py
@@ -0,0 +1,23 @@
+#
+# from flask import Flask
+# import connexion
+#
+#
+# app  = connexion.App(__name__)
+# import pdb; pdb.set_trace()
+# app.add_api('swagger/user_password.yaml')
+# application = app.app
+#
+# @app.route('/')
+# class test:
+#
+#     def getUserDetails(self):
+#         return "Say hello to all"
+#
+#
+# # app.run(host='192.168.1.8', port=4000)
+#
+# # @app.route('/bye')
+# # def Saybye():
+# #     return "Bye see you again"
+#
---
+++ Encrypt_project/run.py
@@ -0,0 +1,6 @@
+
+from password_encryption import app
+
+
+if __name__ == "__main__":
+    app.run(host='192.168.1.8', port=4000)
